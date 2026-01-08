6c51a5bcb6a52fcea219eec7e9676fd3c9245ba8
Tao Zhang <jimmieboy@gmail.com>
2026-01-07 16:02:25 -0800

    Update site title and description in config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Tao Zhang's blog";
+export const SITE_DESCRIPTION = "A blog started in 2026";
